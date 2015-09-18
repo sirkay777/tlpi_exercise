760242ecf12943c6bc41cb36e661a53d98e179ff
Kai <k@wanglvxuan.com>
2015-09-18 21:52:57 +0800

    fix sender

--- 57/1/sender.c
+++ 57/1/sender.c
@@ -1,21 +1,25 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <string.h>
+#include <stdio.h>
 
-#define BUFF_SIZE 10
+#define DATA_SIZE 100
 #define SOCK_PATH "/tmp/receiver_sock"
-#define SEND_DATA "data to send"
 
 int main(int argc, char* argv[])
 {
 	int sfd, count;
+	char data[DATA_SIZE];
 	struct sockaddr_un addr;
 	sfd = socket(AF_UNIX, SOCK_DGRAM, 0);
 	memset(&addr, 0, sizeof(struct sockaddr_un));
 	addr.sun_family = AF_UNIX;
 	strncpy(addr.sun_path, SOCK_PATH, sizeof(addr.sun_path) - 1);
-	for(count =0; count < 1000; count++)
+	for(count =0; ; count++)
 	{
-		sendto(sfd, SEND_DATA, sizeof(SEND_DATA), 0, (struct sockaddr*)&addr, sizeof(struct sockaddr_un));
+		sprintf(data,"%d\n",count);
+		sendto(sfd, data, DATA_SIZE, 0, (struct sockaddr*)&addr, sizeof(struct sockaddr_un));
+		printf("%d\n", count);
+		fflush(NULL);
 	}
 }
